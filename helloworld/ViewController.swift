6eaf55c7a04c458de13f2d5bb805e5d88ee0363f
wbd3 <willdow@Wills-MacBook-Pro-2.local>
2014-09-13 14:56:15 -0600

    Hello World

--- helloworld/ViewController.swift
+++ helloworld/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var messageText: UILabel!
+    @IBAction func buttonPressed(sender: UIButton) {
+        messageText.text="Hello World!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
